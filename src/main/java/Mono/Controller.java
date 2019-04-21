452a15058f4375f877f8fdc863c75f18da6d8cdf
xiaobaicai <xxx@icloud.com>
2019-04-21 14:40:49 +0800

    init

--- src/main/java/Mono/Controller.java
+++ src/main/java/Mono/Controller.java
@@ -70,9 +70,9 @@ public class Controller {
     @RequestMapping(value = "/getRedis")
     public String getRedis(String key) {
         RedissonClient redisson= RedissionClient.getInstance();
-//        RBucket<String> bucket =redisson.getBucket(key);
-//        System.out.println("bucket:"+bucket.get());
-        RAtomicLong rAtomicLong = redisson.getAtomicLong("key");
+        RBucket<Long> bucket =redisson.getBucket(key);
+        System.out.println("bucket:"+bucket.get());
+        RAtomicLong rAtomicLong = redisson.getAtomicLong(key);
         System.out.println("rAtomicLong.get():"+rAtomicLong.get());
 
 
