5f3e03d91cc49928c20df25f3e612cc35073e293
xiaobaicai <xxx@icloud.com>
2019-04-21 15:10:45 +0800

    init

--- src/main/java/Mono/redission/RedissionClient.java
+++ src/main/java/Mono/redission/RedissionClient.java
@@ -2,6 +2,7 @@ package Mono.redission;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
 import org.redisson.config.Config;
 
 public class RedissionClient {
@@ -12,13 +13,13 @@ public class RedissionClient {
             synchronized (RedissionClient.class){
                 if(redissonClient == null){
                     Config config = new Config();
+                    config.setCodec(new StringCodec());
                     config.useClusterServers()
                             .setScanInterval(2000) // cluster state scan interval in milliseconds
                             // use "rediss://" for SSL connection
                             .addNodeAddress("redis://127.0.0.1:7000", "redis://127.0.0.1:7001")
                             .addNodeAddress("redis://127.0.0.1:7002")
                             .setPassword("ijklk~df.oie,80@2");
-
 
                     redissonClient = Redisson.create(config);
                 }
