3ae3e95d2dbaf49ce899e8ed9da0f59d0857f8ed
52706 <527065489@qq.com>
2019-07-07 09:41:48 +0800

    coroutine测试

--- coroutine/src/main/kotlin/com/kotlin/conroutineTestMain.kt
+++ coroutine/src/main/kotlin/com/kotlin/conroutineTestMain.kt
@@ -17,31 +17,23 @@ fun main() {
 suspend fun <T> retryIO(block: suspend ()-> T):T{
     var currDelay = 3000L
 
-        try {
+
             println(Thread.currentThread().name+"-----1111")
-            var ioException: IOException = IOException()
-            throw ioException
-            return block()
-        }catch (e: IOException){
-            e.printStackTrace()
-        }
+
         retryIO2(suspend{println(Thread.currentThread().name+"2222callback")})
-        delay(currDelay)
+        Thread.sleep(currDelay)
         //currDelay = (currDelay * 2).coerceAtMost(60000L)
         return block()
 }
 
 suspend fun <T> retryIO2(block: suspend ()-> T):T{
     var currDelay = 1000L
-    while (true) {
-        try {
+    var count : Int = 0
+    while (count < 3) {
             println(Thread.currentThread().name + "-----2222")
-            return block()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
         delay(currDelay)
+        count++
         //currDelay = (currDelay * 2).coerceAtMost(60000L)
-        return block()
     }
+    return block()
 }
